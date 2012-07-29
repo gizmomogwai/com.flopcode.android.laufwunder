97f93f2b944d7ad2eb16e8d169815bd82b60d795
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-07-29 09:12:20 +0200

    added more workouts

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,8 @@
-task :default do
+desc 'copy all workouts to the device'
+task :copy_to_device do
   Dir.glob('data/*.workout').each do |f|
     sh "adb push #{f} /sdcard/laufwunder/workouts"
   end
 end
+
+task :default => :copy_to_device
