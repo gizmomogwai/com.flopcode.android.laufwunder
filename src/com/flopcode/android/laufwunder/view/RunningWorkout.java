d62e8080134ef2bed2809ebab4c0e0a2512d7525
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-08-05 11:16:42 +0200

    added confirm dialog before stopping workouts

--- src/com/flopcode/android/laufwunder/view/RunningWorkout.java
+++ src/com/flopcode/android/laufwunder/view/RunningWorkout.java
@@ -1,7 +1,10 @@
 package com.flopcode.android.laufwunder.view;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
@@ -18,10 +21,12 @@ import com.flopcode.android.laufwunder.service.WorkoutService.LocalBinder;
 
 public class RunningWorkout extends Activity {
 
+	protected static final int CONFIRM_DIALOG = 1;
+
 	private TextView fTitle;
 
 	private TextView fTotalTime;
-	
+
 	private TextView fCurrentInterval;
 
 	private Button fStop;
@@ -34,7 +39,7 @@ public class RunningWorkout extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_running_workout);
-		
+
 		fTitle = (TextView) findViewById(R.id.workout_title);
 		fTotalTime = (TextView) findViewById(R.id.total_time);
 		fIntervals = (IntervalsView) findViewById(R.id.intervals);
@@ -45,10 +50,34 @@ public class RunningWorkout extends Activity {
 		fStop.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
 				if (fService != null) {
-					fService.cancelWorkout();
+					showDialog(CONFIRM_DIALOG);
 				}
 			}
 		});
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch (id) {
+		case CONFIRM_DIALOG:
+			dialog = createConfirmDialog();
+		}
+		return dialog;
+	}
+
+	private Dialog createConfirmDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Really stop workout?").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				fService.cancelWorkout();
+			}
+		}).setNegativeButton("No", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+		return builder.create();
 	}
 
 	@Override
@@ -56,14 +85,14 @@ public class RunningWorkout extends Activity {
 		super.onResume();
 		bindService(getServiceIntent(), fServiceConnection, 0);
 	}
-	
+
 	@Override
 	protected void onPause() {
 		if (fService != null) {
 			fService.removeListener(fWorkoutServiceListener);
 		}
 		unbindService(fServiceConnection);
-		
+
 		super.onPause();
 	}
 
@@ -86,16 +115,16 @@ public class RunningWorkout extends Activity {
 		if (fService != null) {
 			Workout workout = fService.getWorkout();
 			fTitle.setText(workout.fTitle);
-			
-			int secondsOfWorkout = (int)((workout.fIntervals.getDurationInMs() * fService.getPercentage()) / 1000);
+
+			int secondsOfWorkout = (int) ((workout.fIntervals.getDurationInMs() * fService.getPercentage()) / 1000);
 			int minutes = secondsOfWorkout / 60;
 			int seconds = secondsOfWorkout % 60;
 			fTotalTime.setText(String.format("TotalTime: %d:%d", minutes, seconds));
 			fIntervals.setIntervals(workout.fIntervals);
 			fIntervals.setPercentage(fService.getPercentage());
-			
+
 			fCurrentInterval.setText(fService.getCurrentInterval().fComment);
-			fCurrentPercentage.setText(String.format("%d%%", (int)(100 * fService.getCurrentIntervalPercentage())));
+			fCurrentPercentage.setText(String.format("%d%%", (int) (100 * fService.getCurrentIntervalPercentage())));
 		}
 	}
 
@@ -106,7 +135,6 @@ public class RunningWorkout extends Activity {
 				public void run() {
 					updateUi();
 				}
-
 			});
 		}
 	};
@@ -126,5 +154,4 @@ public class RunningWorkout extends Activity {
 			fStop.setEnabled(false);
 		}
 	};
-
 }
